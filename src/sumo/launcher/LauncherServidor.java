47d024a07214181accf45e8dd880b309426e0d1c
Sneyder <juansneydermendez@gmail.com>
2026-03-15 13:51:04 -0500

    Primer subida de archivos, falta revisar cometario y pulirlos y el diagramita

--- src/sumo/launcher/LauncherServidor.java
+++ src/sumo/launcher/LauncherServidor.java
@@ -4,7 +4,7 @@ import sumo.controlador.ControladorServidor;
 import javax.swing.SwingUtilities;
 
 /**
- * Punto de entrada de la aplicación servidor del Combate de Sumo.
+ * Punto de entrada de la aplicacion servidor del Combate de Sumo.
  * <p>
  * Esta clase contiene únicamente el método {@code main}, el cual delega
  * la inicialización al método estático {@link ControladorServidor#iniciar()}.
